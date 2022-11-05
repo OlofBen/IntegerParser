91f9ad330cdb17fe1d28958ec9827b850edbf5c7
Olof Bengtsson <ol1058be-s@student.lu.se>
2022-11-05 11:04:05 +0100

    first commit

---
+++ app/src/main/java/integerparser/App.java
@@ -0,0 +1,12 @@
+package integerparser;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        new App().run();
+    }
+
+    private void run() {
+    }
+}
